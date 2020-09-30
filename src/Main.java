d7927da14b21aef03c892f1f9ec15501b02fed45
Bruna <42246566+brunacmelo@users.noreply.github.com>
2020-09-30 15:54:14 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -6,3 +6,4 @@ class Main {
 	  test.testFrancMultiplication();
   }
 }
+
--- src/Money.java
+++ src/Money.java
@@ -1,25 +1,36 @@
-class Money {
+abstract class Money {
   protected int amount;
+
+  static Dollar dollar(int amount) {
+    return new Dollar(amount);
+  }
+   
+  static Money franc(int amount) {
+    return new Franc(amount);
+  }
+
+  abstract Money times(int multiplier);
+
   public boolean equals(Object object) {
     Money money = (Money) object;
-    return amount == money.amount;
+    return amount == money.amount && getClass().equals(money.getClass());
   }
 }
 
 class Dollar extends Money {
   Dollar(int amount) {
-    this.amount= amount;
+    this.amount = amount;
   }
-  Dollar times(int multiplier) {
+  Money times(int multiplier)  {
     return new Dollar(amount * multiplier);
-  }
+  }	
 }
 
 class Franc extends Money {  			
   Franc(int amount) {      
     this.amount= amount;
   }					
-  Franc times(int multiplier) {      
+  Money times(int multiplier) {      
     return new Franc(amount * multiplier);					
   }   				
 }
